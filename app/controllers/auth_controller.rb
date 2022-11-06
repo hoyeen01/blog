3164f1e8445352bcf9eff2ee06ab9821f14e2ebf
Oyinkansola Salami <kheerah102@gmail.com>
2022-11-06 04:09:59 +0100

    added authentication

---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,46 @@
+class AuthController < ApplicationController 
+  before_action :requires_sign_in, only: [:logout]
+
+  def sign_in
+  end
+
+  def sign_up
+  end
+
+  def login
+    credentials = params[:user]
+    user = User.find_by(email: credentials[:email])
+    if user.present?
+      if user.authenticate(credentials[:password])
+        session[:user] = user.id
+        redirect_to articles_path
+      else
+        flash[:error] = 'Password is not correct'
+        render "auth/sign_in"
+      end
+    else
+      flash[:error] = 'User with email does not exist'
+      render "auth/sign_in"
+    end
+  end
+
+  def create_user
+    user_params = params[:user].permit!
+    if User.find_by(email: user_params[:email]).present?
+      flash[:error] = 'User with email already exists'
+      render 'auth/sign_up'
+    else
+      user = User.create(user_params.except(:confirm_password))
+      if user.errors&.full_messages.present?
+        flash[:error] = user.errors&.full_messages[0]
+        return render 'auth/sign_up'
+      end
+      redirect_to sign_in_path
+    end
+  end
+
+  def logout
+    session[:user] = nil
+    redirect_to login_path
+  end 
+end
